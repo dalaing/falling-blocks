384aee661788a0078e8ffffd5ff0e8c6c32097d6
Dave Laing <dave.laing.80@gmail.com>
2015-09-14 11:20:28 +1000

    Restructure code directory

--- code/post1/mtl/shell.nix
+++
@@ -1,8 +0,0 @@
-with (import <nixpkgs> {}).pkgs;
-let modifiedHaskellPackages = haskellPackages.override {
-      overrides = self: super: {
-        sdl2 = self.callPackage ../../../../sdl2 {};
-        falling-blocks-mtl = self.callPackage ./. {};
-      };
-    };
-in modifiedHaskellPackages.falling-blocks-mtl.env
